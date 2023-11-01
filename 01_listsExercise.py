2b9b78e81980d7dc78f765ccb44ed059d36053f8
Guilherme Marins <guilhermebmarins@gmail.com>
2023-11-01 17:16:32 -0300

    Exercise of lists

---
+++ 01_listsExercise.py
@@ -0,0 +1,15 @@
+listOfMaterials = ['pen', 'book', 'pencil', 'journal']
+print(listOfMaterials)
+
+listOfMaterials.append('eraser')
+print(listOfMaterials)
+
+listOfMaterials.sort()
+print(listOfMaterials)
+
+print('List size = ' + str(len(listOfMaterials)))
+
+listOfMaterials.remove('journal')
+print(listOfMaterials)
+
+print('List size = ' + str(len(listOfMaterials)))
