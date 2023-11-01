83421088509d43ccfc2a7dc85b07f52168243c12
Guilherme Marins <guilhermebmarins@gmail.com>
2023-11-01 17:36:47 -0300

    for

---
+++ 02_for.py
@@ -0,0 +1,11 @@
+listOfMaterials = ['pen', 'book', 'pencil', 'journal']
+print(listOfMaterials)
+
+for material in listOfMaterials:
+    print(material)
+    print(material.title())
+
+print(listOfMaterials)
+
+for number in range(1,5):
+    print(number)
